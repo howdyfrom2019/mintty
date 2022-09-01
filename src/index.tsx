8aceb649d636504c7d88622b355b8e829a317a15
Russel <tjd1emd@naver.com>
2022-09-01 22:27:02 +0900

    change contents

--- src/index.tsx
+++ src/index.tsx
@@ -11,12 +11,12 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ThemeProvider>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <App/>
+    </ThemeProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/layout/Wallpaper/styles.tsx
+++ src/layout/Wallpaper/styles.tsx
@@ -1,3 +1,9 @@
+/** @jsxImportSource @emotion/react */
 import styled from "@emotion/styled";
 
-export const WallpaperLayout = styled.div``;
+export const WallpaperLayout = styled.div`
+  color: ${({ theme }) => {
+    console.log(theme);
+    return theme.colors.black;
+  }};
+`;

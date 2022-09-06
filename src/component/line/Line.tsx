cca14b7cb9b49c34753afed59f60505ed56125bc
howdyfrom2019 <tjd1emd@naver.com>
2022-09-06 13:37:26 +0900

    line component

---
+++ src/component/line/Line.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Line = () => {
+
+}
+
+export default Line;
---
+++ src/component/line/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const LineWrapper = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  
+`;
+const LineDivider = styled.div<{ isVertical?: boolean }>`
+  
+`;
